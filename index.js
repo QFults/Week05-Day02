e9c31433c2142496ba6be6b31571bed31ddc353f
Quinton Fults <quinton.fults.work@gmail.com>
2021-03-03 13:10:03 -0800

    code

--- index.js
+++ index.js
@@ -6,6 +6,30 @@
 
 // console.log(movies)
 
-const chalk = require('chalk')
+// const chalk = require('chalk')
 
-console.log(chalk.blue('Hello World!'))
+// console.log(chalk.blue('Hello World!'))
+
+const inquirer = require('inquirer')
+
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'person',
+    message: 'What is your name?'
+  }, {
+    type: 'number',
+    name: 'age',
+    message: 'How old are you?'
+  },
+  {
+    type: 'list',
+    name: 'pet',
+    message: 'Choose your favorite type of pet',
+    choices: ['dog', 'cat', 'lizard', 'bird', 'snail']
+  }
+])
+  .then(res => {
+    console.log(res)
+  })
+  .catch(err => console.log(err))
